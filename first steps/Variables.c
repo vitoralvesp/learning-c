8d66c127f244d4772c45cacf25ddd77c48359703
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-08-13 12:57:59 -0300

    Adding a constant variable to the program

--- first steps/Variables.c
+++ first steps/Variables.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    
+    const int number = 10; // Create a constant variable
     int variable = 5; // Create a variable called "variable" that returns a integer number "5"
     float floatVariable = 1.5; // Create a variable called "floatVariable" that returns a float number "1.5"
     double doubleVariable = 10.11; // Create a variable called "doubleVariable" that returns a double value "10.11"
@@ -10,6 +10,7 @@ int main()
     char greetings[] = "Hello World"; // Create a variable called "greetings" that returns a string "Hello World"; obs: to create a long term string you have to use [] after the name of variables, because it's a array.
 
 
+    printf("%d\n", number);
     printf("%d\n", variable); // Sintaxe to print numbers to the console!
     printf("%f\n", floatVariable); // Sintaxe to print float numbers to the console!
     printf("%lf\n", doubleVariable); // Sintaxe to print double "float" numbers to the console!
