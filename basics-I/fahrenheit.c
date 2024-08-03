12eb2b5e5a1f6d936e9dc21dd00fb52a1bde19c5
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2024-08-03 17:07:05 -0300

    update: new comment on top and the numbers are being printed right-justified

--- basics-I/fahrenheit.c
+++ basics-I/fahrenheit.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/* print Fahrenheit-Celsius table 
+	for fahr =  0, 20, ..., 300 */
 int main() {
 	
 	int fahr, celsius;
@@ -12,7 +14,7 @@ int main() {
 	fahr = lower;
 	while (fahr <= upper) {
 		celsius = 5 * (fahr - 32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		printf("%3d\t%6d\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 	
